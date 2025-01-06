0f4e8378a530a939af3ffd4a28c2254a49d3fea3
jitendra977 <jitendrakhadka4@gmail.com>
2025-01-06 10:24:55 +0900

    server app turn on and off led using toggle

--- server/views.py
+++ server/views.py
@@ -80,3 +80,36 @@ class LEDViewSet(viewsets.ModelViewSet):
         
         except ValueError:
             return Response({"error": "Invalid GPIO pin number"}, status=400)
+    
+    @action(detail=True, methods=['post'])
+    def toggle(self, request, pk=None):
+        """Toggle the LED state (on/off) for a specific LED instance using the gpio pin."""
+        try:
+            # Retrieve the LED object based on the pk
+            led_instance = self.get_object()
+            gpio_pin = led_instance.gpio
+
+            if gpio_pin == 0:
+                return Response({"error": "Invalid GPIO pin"}, status=400)
+
+            # Check if the LED instance already exists in the dictionary
+            if gpio_pin not in led_instances:
+                led_instances[gpio_pin] = GPIOLED(gpio_pin)
+
+            # Toggle the LED state
+            led = led_instances[gpio_pin]
+            if led.is_lit:
+                led.off()
+                led_instance.status = False
+                status = "off"
+            else:
+                led.on()
+                led_instance.status = True
+                status = "on"
+
+            led_instance.save()
+
+            return Response({"status": f"LED on GPIO pin {gpio_pin} is now {status}"})
+
+        except Exception as e:
+            return Response({"error": str(e)}, status=500)
